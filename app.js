fba7034288a97acb86eacf89ce61269b2eb4ecf1
Arnaud Lachaume <arnaud.lachaume@maestrano.com>
2016-02-14 23:25:44 +1100

    add web authentication

--- app.js
+++ app.js
@@ -1,37 +1,18 @@
 var express = require('express');
 var http = require('http');
-var https = require('https');
 var path = require('path');
 var server = require('socket.io');
 var pty = require('pty.js');
 var fs = require('fs');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var passportSocketIo = require("passport.socketio");
 
+//===============================================
+// Server Configuration
+//===============================================
 var opts = require('optimist')
     .options({
-        sslkey: {
-            demand: false,
-            description: 'path to SSL key'
-        },
-        sslcert: {
-            demand: false,
-            description: 'path to SSL certificate'
-        },
-        sshhost: {
-            demand: false,
-            description: 'ssh server host'
-        },
-        sshport: {
-            demand: false,
-            description: 'ssh server port'
-        },
-        sshuser: {
-            demand: false,
-            description: 'ssh user'
-        },
-        sshauth: {
-            demand: false,
-            description: 'defaults to "password", you can use "publickey,password" instead'
-        },
         port: {
             demand: true,
             alias: 'p',
@@ -39,83 +20,134 @@ var opts = require('optimist')
         },
     }).boolean('allow_discovery').argv;
 
-var runhttps = false;
-var sshport = 22;
-var sshhost = 'localhost';
-var sshauth = 'password';
-var globalsshuser = '';
-
-if (opts.sshport) {
-    sshport = opts.sshport;
-}
-
-if (opts.sshhost) {
-    sshhost = opts.sshhost;
-}
-
-if (opts.sshauth) {
-	sshauth = opts.sshauth
-}
-
-if (opts.sshuser) {
-    globalsshuser = opts.sshuser;
-}
-
-if (opts.sslkey && opts.sslcert) {
-    runhttps = true;
-    opts['ssl'] = {};
-    opts.ssl['key'] = fs.readFileSync(path.resolve(opts.sslkey));
-    opts.ssl['cert'] = fs.readFileSync(path.resolve(opts.sslcert));
-}
-
 process.on('uncaughtException', function(e) {
     console.error('Error: ' + e);
 });
 
-var httpserv;
+//===============================================
+// Passport Configuration
+//===============================================
+passport.use(new LocalStrategy(
+  function(username, password, cb) {
+    // if (username != "foo" || password != "bar") {
+    //   console.log("Failed login");
+    //   return cb(null, false, { message: 'Incorrect password.' });
+    // }
+    console.log("Successful login");
+    return cb(null, { id: username });
+  }
+));
 
-var app = express();
-app.get('/wetty/ssh/:user', function(req, res) {
-    res.sendfile(__dirname + '/public/wetty/index.html');
+// Configure Passport authenticated session persistence.
+passport.serializeUser(function(user, cb) {
+  cb(null, user.id);
 });
-app.use('/', express.static(path.join(__dirname, 'public')));
 
-if (runhttps) {
-    httpserv = https.createServer(opts.ssl, app).listen(opts.port, function() {
-        console.log('https on port ' + opts.port);
-    });
-} else {
-    httpserv = http.createServer(app).listen(opts.port, function() {
-        console.log('http on port ' + opts.port);
-    });
-}
+passport.deserializeUser(function(id, cb) {
+  cb(null, { id: id });
+});
 
+//===============================================
+// Express Configuration
+//===============================================
+var app = express();
+
+// Configure view engine to render EJS templates.
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// Define session and session store
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+var sessionStore = new FileStore();
+
+// Configure session middleware
+var sessionConfig = {
+  key: 'webconsole',
+  secret: '2asd7f4d6s15s74d',
+  resave: false,
+  saveUninitialized: true,
+  store: sessionStore//,
+  //cookie: { secure: true }
+};
+var sessionMiddleware = session(sessionConfig);
+
+// Configure cookie
+var cookieParser = require('cookie-parser');
+//var cookieMiddleware = require('cookie-parser')();
+
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+app.use(require('morgan')('combined'));
+app.use(cookieParser());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(sessionMiddleware);
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/wetty', express.static(path.join(__dirname, 'public','wetty')));
+
+app.get('/login',
+  function(req, res){
+    console.log("Rendering login");
+    res.render('login');
+  });
+
+app.post('/login',
+  passport.authenticate('local', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+app.get('/',
+  require('connect-ensure-login').ensureLoggedIn(),
+  function(req, res){
+    res.render('index', { user: req.user });
+  });
+
+//===============================================
+// Initiate http server
+//===============================================
+var httpserv;
+httpserv = http.createServer(app).listen(opts.port, function() {
+    console.log('http on port ' + opts.port);
+});
+
+//===============================================
+// Socket server
+//===============================================
 var io = server(httpserv,{path: '/wetty/socket.io'});
+
+io.use(passportSocketIo.authorize({
+  cookieParser: cookieParser,
+  key:          sessionConfig.key,
+  secret:       sessionConfig.secret,
+  store:        sessionStore,
+  fail: function(data, message, error, accept) {
+    if (error) accept(new Error(message));
+  },
+  success: function(data, accept) {
+    console.log("success socket.io auth");
+    accept();
+  }
+}));
+
 io.on('connection', function(socket){
-    var sshuser = '';
     var request = socket.request;
     console.log((new Date()) + ' Connection accepted.');
-    if (match = request.headers.referer.match('/wetty/ssh/.+$')) {
-        sshuser = match[0].replace('/wetty/ssh/', '') + '@';
-    } else if (globalsshuser) {
-        sshuser = globalsshuser + '@';
-    }
 
+    // Initiate session
     var term;
-    if (process.getuid() == 0) {
-        term = pty.spawn('/bin/login', [], {
-            name: 'xterm-256color',
-            cols: 80,
-            rows: 30
-        });
-    } else {
-        term = pty.spawn('ssh', [sshuser + sshhost, '-p', sshport, '-o', 'PreferredAuthentications=' + sshauth], {
-            name: 'xterm-256color',
-            cols: 80,
-            rows: 30
-        });
-    }
-    console.log((new Date()) + " PID=" + term.pid + " STARTED on behalf of user=" + sshuser)
+    term = pty.spawn('/bin/bash', [], {
+        name: 'xterm-256color',
+        cols: 80,
+        rows: 30
+    });
+
+    // Loop
     term.on('data', function(data) {
         socket.emit('output', data);
     });
